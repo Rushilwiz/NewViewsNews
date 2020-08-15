4412825950cc979cecc2ec997271c46ce5f8e9e2
Rushil Umaretiya <rushilwiz@gmail.com>
2020-08-15 01:23:04 -0400

    got the news app up and created templates and css

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about (request):
+    return render (request, 'news/about.html')
+
+def home (request):
+    return render (request, 'news/home.html')
