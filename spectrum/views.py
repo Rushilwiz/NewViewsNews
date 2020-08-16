7af0c195387036dc3ccddd3056cb510631b979fd
Rushil Umaretiya <rushilwiz@gmail.com>
2020-08-16 11:05:01 -0400

    fixed spectrum

--- spectrum/views.py
+++ spectrum/views.py
@@ -16,7 +16,17 @@ def right(request):
 
 
 def left(request):
-    pass
+    feed = feedparser.parse("https://rss.app/feeds/xC2p60TEA2cjxsTp.xml")
+    print(feed.entries)
+    context = {
+        "feed": feed.entries
+    }
+    return render (request, "spectrum/feed.html", context)
 
 def center(request):
-    pass
+    feed = feedparser.parse("https://rss.app/feeds/H4872WxBBmcWNjWr.xml")
+    print(feed.entries)
+    context = {
+        "feed": feed.entries
+    }
+    return render (request, "spectrum/feed.html", context)
